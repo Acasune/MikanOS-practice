cc1a480ed6659bbfbb31c1e9f2ab4427aa6b7d0e
Acasune <tttt4aaaa@gmail.com>
2021-08-09 15:09:57 +0900

    Done: osbook_day12d

--- kernel/usb/classdriver/keyboard.hpp
+++ kernel/usb/classdriver/keyboard.hpp
@@ -1,15 +1,10 @@
-/**
- * @file usb/classdriver/keyboard.hpp
- *
- * HID keyboard class driver.
- */
-
 #pragma once
 
 #include <functional>
 #include "usb/classdriver/hid.hpp"
 
 namespace usb {
+  // #@@range_begin(hid_keyboard)
   class HIDKeyboardDriver : public HIDBaseDriver {
    public:
     HIDKeyboardDriver(Device* dev, int interface_index);
@@ -19,7 +14,7 @@ namespace usb {
 
     Error OnDataReceived() override;
 
-    using ObserverType = void (uint8_t keycode);
+    using ObserverType = void (uint8_t modifier, uint8_t keycode);
     void SubscribeKeyPush(std::function<ObserverType> observer);
     static std::function<ObserverType> default_observer;
 
@@ -27,6 +22,7 @@ namespace usb {
     std::array<std::function<ObserverType>, 4> observers_;
     int num_observers_ = 0;
 
-    void NotifyKeyPush(uint8_t keycode);
+    void NotifyKeyPush(uint8_t modifier, uint8_t keycode);
   };
+  // #@@range_end(hid_keyboard)
 }
