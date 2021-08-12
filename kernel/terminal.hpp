69c34bea1783b10c2c1e36d10a1922c72b1a27ca
Acasune <tttt4aaaa@gmail.com>
2021-08-12 15:22:18 +0900

    Done: osbook_day18b

--- kernel/terminal.hpp
+++ kernel/terminal.hpp
@@ -5,6 +5,7 @@
 #include "window.hpp"
 #include "task.hpp"
 #include "layer.hpp"
+#include "fat.hpp"
 
 class Terminal {
   public:
@@ -31,6 +32,7 @@ class Terminal {
     void Scroll1();
     
     void ExecuteLine();
+    void ExecuteFile(const fat::DirectoryEntry& file_entry);
     void Print(const char* s);
     void Print(char c);
 
