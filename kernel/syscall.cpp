a6ac1c15af177e08c4f624822852e1ebee60516a
Acasune <tttt4aaaa@gmail.com>
2021-08-14 14:44:02 +0900

    Done: osbook_day22b,c

--- kernel/syscall.cpp
+++ kernel/syscall.cpp
@@ -10,6 +10,7 @@
 #include "task.hpp"
 #include "terminal.hpp"
 #include "font.hpp"
+#include "timer.hpp"
 
 namespace syscall {
   struct Result {
@@ -27,9 +28,12 @@ SYSCALL(LogString) {
     return { 0, EPERM };
   }
   const char* s = reinterpret_cast<const char*>(arg2);
-
+  const auto len = strlen(s);
+  if (len > 1024) {
+    return { 0, E2BIG };
+  }
   Log(static_cast<LogLevel>(arg1), "%s", s);
-  return { 0, E2BIG };
+  return { len, 0 };
 }
 
 SYSCALL(PutString) {
@@ -42,7 +46,7 @@ SYSCALL(PutString) {
 
   if (fd == 1) {
     const auto task_id = task_manager->CurrentTask().ID();
-    (*terminals)[task_id] ->Print(s, len);
+    (*terminals)[task_id]->Print(s, len);
     return { len, 0 };
   }
   return { 0, EBADF };
@@ -58,39 +62,64 @@ SYSCALL(Exit) {
 SYSCALL(OpenWindow) {
   const int w = arg1, h = arg2, x = arg3, y = arg4;
   const auto title = reinterpret_cast<const char*>(arg5);
-  const auto win = std::make_shared<ToplevelWindow>(w, h, screen_config.pixel_format, title);
+  const auto win = std::make_shared<ToplevelWindow>(
+      w, h, screen_config.pixel_format, title);
 
   __asm__("cli");
   const auto layer_id = layer_manager->NewLayer()
     .SetWindow(win)
     .SetDraggable(true)
-    .Move({x,y})
+    .Move({x, y})
     .ID();
   active_layer->Activate(layer_id);
-  __asm__("sti") ;
+  __asm__("sti");
 
   return { layer_id, 0 };
 }
 
-SYSCALL(WinWriteString) {
-  const unsigned int layer_id = arg1;
-  const int x = arg2, y = arg3;
-  const uint32_t color = arg4;
-  const auto s = reinterpret_cast<const char*>(arg5);
+namespace {
+  template <class Func, class... Args>
+  Result DoWinFunc(Func f, unsigned int layer_id, Args... args) {
+    __asm__("cli");
+    auto layer = layer_manager->FindLayer(layer_id);
+    __asm__("sti");
+    if (layer == nullptr) {
+      return { 0, EBADF };
+    }
 
-  __asm__("cli");
-  auto layer = layer_manager->FindLayer(layer_id);
-  __asm__("sti");
-  if (layer==nullptr) {
-    return { 0, EBADF };
+    const auto res = f(*layer->GetWindow(), args...);
+    if (res.error) {
+      return res;
+    }
+
+    __asm__("cli");
+    layer_manager->Draw(layer_id);
+    __asm__("sti");
+
+    return res;
   }
+}
 
-  WriteString(*layer->GetWindow()->Writer(), {x,y}, s, ToColor(color));
-  __asm__("cli");
-  layer_manager->Draw(layer_id);
-  __asm__("sti");
+SYSCALL(WinWriteString) {
+  return DoWinFunc(
+      [](Window& win,
+         int x, int y, uint32_t color, const char* s) {
+        WriteString(*win.Writer(), {x, y}, s, ToColor(color));
+        return Result{ 0, 0 };
+      }, arg1, arg2, arg3, arg4, reinterpret_cast<const char*>(arg5));
+}
 
-  return { 0, 0 };
+SYSCALL(WinFillRectangle) {
+  return DoWinFunc(
+      [](Window& win,
+         int x, int y, int w, int h, uint32_t color) {
+        FillRectangle(*win.Writer(), {x, y}, {w, h}, ToColor(color));
+        return Result{ 0, 0 };
+      }, arg1, arg2, arg3, arg4, arg5, arg6);
+}
+
+SYSCALL(GetCurrentTick) {
+  return { timer_manager->CurrentTick(), kTimerFreq };
 }
 
 #undef SYSCALL
@@ -99,12 +128,14 @@ SYSCALL(WinWriteString) {
 
 using SyscallFuncType = syscall::Result (uint64_t, uint64_t, uint64_t,
                                          uint64_t, uint64_t, uint64_t);
-extern "C" std::array<SyscallFuncType*, 5> syscall_table{
+extern "C" std::array<SyscallFuncType*, 7> syscall_table{
   /* 0x00 */ syscall::LogString,
   /* 0x01 */ syscall::PutString,
   /* 0x02 */ syscall::Exit,
   /* 0x03 */ syscall::OpenWindow,
   /* 0x04 */ syscall::WinWriteString,
+  /* 0x05 */ syscall::WinFillRectangle,
+  /* 0x06 */ syscall::GetCurrentTick,
 };
 
 void InitializeSyscall() {
