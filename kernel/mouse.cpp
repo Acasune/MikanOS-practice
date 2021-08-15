457f24d48245e6a7ac887a12b2fe8ade78fb0534
Acasune <tttt4aaaa@gmail.com>
2021-08-15 10:22:53 +0900

    Done: osbook_day23a

--- kernel/mouse.cpp
+++ kernel/mouse.cpp
@@ -5,6 +5,7 @@
 #include "graphics.hpp"
 #include "layer.hpp"
 #include "usb/classdriver/mouse.hpp"
+#include "task.hpp"
 
 namespace {
   const char mouse_cursor_shape[kMouseCursorHeight][kMouseCursorWidth + 1] = {
@@ -33,6 +34,30 @@ namespace {
     "         @.@   ",
     "         @@@   ",
   };
+
+  void SendMouseMessage(Vector2D<int> newpos, Vector2D<int> posdiff, uint8_t buttons) {
+    const auto act = active_layer->GetActive();
+    if (!act) {
+      return;
+    }
+    const auto layer = layer_manager->FindLayer(act);
+
+    const auto task_it = layer_task_map->find(act);
+    if (task_it == layer_task_map->end()) {
+      return;
+    }
+
+    if (posdiff.x != 0 || posdiff.y != 0) {
+      const auto relpos = newpos - layer->GetPosition();
+      Message msg{Message::kMouseMove};
+      msg.arg.mouse_move.x = relpos.x;
+      msg.arg.mouse_move.y = relpos.y;
+      msg.arg.mouse_move.dx = posdiff.x;
+      msg.arg.mouse_move.dy = posdiff.y;
+      msg.arg.mouse_move.buttons = buttons;
+      task_manager->SendMessage(task_it->second, msg);
+    }
+  }
 }
 
 void DrawMouseCursor(PixelWriter* pixel_writer, Vector2D<int> position) {
@@ -67,7 +92,6 @@ void Mouse::OnInterrupt(uint8_t buttons, int8_t displacement_x, int8_t displacem
 
   layer_manager->Move(layer_id_, position_);
 
-  // #@@range_begin(mouse_handler)
   const bool previous_left_pressed = (previous_buttons_ & 0x01);
   const bool left_pressed = (buttons & 0x01);
   if (!previous_left_pressed && left_pressed) {
@@ -79,12 +103,15 @@ void Mouse::OnInterrupt(uint8_t buttons, int8_t displacement_x, int8_t displacem
       active_layer->Activate(0);
     }
   } else if (previous_left_pressed && left_pressed) {
-  // #@@range_end(mouse_handler)
     if (drag_layer_id_ > 0) {
       layer_manager->MoveRelative(drag_layer_id_, posdiff);
     }
   } else if (previous_left_pressed && !left_pressed) {
     drag_layer_id_ = 0;
+  }
+
+  if (drag_layer_id_ == 0) {
+    SendMouseMessage(newpos, posdiff, buttons);
   }
 
   previous_buttons_ = buttons;
@@ -109,7 +136,6 @@ void InitializeMouse() {
       mouse->OnInterrupt(buttons, displacement_x, displacement_y);
     };
 
-  // #@@range_begin(set_mouse_layer)
   active_layer->SetMouseLayer(mouse_layer_id);
-  // #@@range_end(set_mouse_layer)
+
 }
